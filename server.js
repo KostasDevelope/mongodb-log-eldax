0375b8d85ed4834700d3ca055f0e5c3659684494
dmtsit\kguselnikov <kguselnikov@techniserv-it.cz>
2025-01-10 16:50:23 +0200

    sevice

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const MovieRoutes = require('./routes/movie-routes');  
 
 const port = 3000;
-const url = 'mongodb://localhost:27017/moviebox';
+const url = 'mongodb://srvnt68-isrtsql:27017/moviebox';
 const app = express();
 app.use(express.json());
 app.use(MovieRoutes);
