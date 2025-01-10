bdbf8abd1f8fa562bb24a9164cab33f63e1263f5
dmtsit\kguselnikov <kguselnikov@techniserv-it.cz>
2025-01-10 20:48:15 +0200

    corse

--- routes/swagger-routes.js
+++ routes/swagger-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express')
 const fs = require('fs');
 
-const swaggerDocument =  JSON.parse(fs.readFileSync('./swagger/output.json'));
+const swaggerDocument = JSON.parse(fs.readFileSync('./swagger/output.json'));
 const SwaggerRoutes = express.Router();
 SwaggerRoutes.use('/api-doc', swaggerUi.serve);
 SwaggerRoutes.get('/api-doc', swaggerUi.setup(swaggerDocument));
