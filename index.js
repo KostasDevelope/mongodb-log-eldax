af162f9c6b564b1164c718edc52b23008fa931a0
KostasDevelope <38208548+KostasDevelope@users.noreply.github.com>
2025-01-11 21:17:19 +0200

    config

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const nconf = require('nconf');
 const mongoose = require('mongoose');
 const MovieRoutes = require('./routes/movie-routes');  
 const SwaggerRoutes = require('./routes/swagger-routes');  
-
+conf.argv().env().file({ file: 'config.json' });
 
 //https://github.com/YauhenKavalchuk/mongodb/blob/lesson_20/server.js
-const port = 3000;
+const port = nconf.get('port');;
 //const url = 'mongodb://srvnt68-isrtsql:27017/moviebox';
-const url = 'mongodb://localhost:27017/moviebox';
+const url = nconf.get('url-mongodb');
 const app = express();
 
 app.use(cors());
