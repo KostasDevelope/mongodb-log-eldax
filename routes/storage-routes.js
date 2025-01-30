f1dbc38f87722742f1f89ecb3b7ce6a4ba96396f
dmtsit\kguselnikov <kguselnikov@techniserv-it.cz>
2025-01-30 16:59:24 +0200

    test app

--- routes/storage-routes.js
+++ routes/storage-routes.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
 const hendleError = (res,error) => { res.status(500).json(error); };
+
 /**
  * @swagger
  * /api/zurnalcinnosti/ulozcinnost:
  *    post:
  *      tags:
  *         - Metody
+ *      requestBody:
+ *        required: true
+ *        content:
+ *           application/json:
+ *              schema:
+ *                 type: object
+ *                 properties:
+ *                   cinnost:
+ *                     type: string
  *      responses:
- *         201:
- *           description: Created new Metody
+ *         200:
+ *           description: OK
  */
 router.post('/api/zurnalcinnosti/ulozcinnost', (req, res) => {  
     try {
-        res.status(200).send('POST request OK!!');;
+        res.status(200).send(req.body.cinnost);
     } catch(error){
         hendleError(res, error);
     } 
 });
 
 module.exports = router;
+
+//https://swagger.io/docs/specification/v2_0/describing-request-body/
